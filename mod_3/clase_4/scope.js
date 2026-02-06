5dcb7213c044c0e7ab4edbfada4c8f388083856c
Mackaber <mkbravo555@gmail.com>
2026-02-05 19:11:54 -0600

    Clase Jueves 5

---
+++ mod_3/clase_4/scope.js
@@ -0,0 +1,20 @@
+let persona = "Juan"
+
+function escuela() {
+    let estudiante = "Carlos";
+
+    console.log(persona)
+    console.log(estudiante)
+}
+
+escuela()
+//console.log(estudiante)
+
+
+let x = 1;
+function myFunc() {
+    let x = 2;
+    console.log(x) // 2
+}
+myFunc() 
+console.log(x) // 1
