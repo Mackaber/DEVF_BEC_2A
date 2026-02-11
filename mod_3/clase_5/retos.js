5f21dacffed0888186ad81499aea210649a335a5
Mackaber <mkbravo555@gmail.com>
2026-02-10 20:26:14 -0600

    Clase Martes 10

--- mod_3/clase_5/retos.js
+++ mod_3/clase_5/retos.js
@@ -1,84 +1,86 @@
 const avengers = [
-    { name: 'Iron Man', age: 48, movies: ['Iron Man', 'The Avengers', 'Avengers: Endgame'] },
-    { name: 'Captain America', age: 101, movies: ['Captain America: The First Avenger', 'The Avengers', 'Avengers: Endgame'] },
-    { name: 'Thor', age: 1500, movies: ['Thor', 'The Avengers', 'Avengers: Endgame'] },
-    { name: 'Black Widow', age: 35, movies: ['Iron Man 2', 'The Avengers', 'Avengers: Endgame'] }
+  {
+    name: "Iron Man",
+    age: 48,
+    movies: ["Iron Man", "The Avengers", "Avengers: Endgame"],
+  },
+  {
+    name: "Captain America",
+    age: 101,
+    movies: [
+      "Captain America: The First Avenger",
+      "The Avengers",
+      "Avengers: Endgame",
+    ],
+  },
+  {
+    name: "Thor",
+    age: 1500,
+    movies: ["Thor", "The Avengers", "Avengers: Endgame"],
+  },
+  {
+    name: "Black Widow",
+    age: 35,
+    movies: ["Iron Man 2", "The Avengers", "Avengers: Endgame"],
+  },
 ];
 
 // Acceder a la segunda película de Thor
-console.log(avengers[2].movies[1])
+console.log(avengers[2].movies[1]);
 
 const rocky_movies = {
-    "Rocky": {
-        "year": 1976,
-        "director": "John G. Avildsen"
-    },
-    "Rocky II": {
-        "year": 1979,
-        "director": "Sylvester Stallone"
-    },
-    "Rocky III": {
-        "year": 1982,
-        "director": "Sylvester Stallone"
-    }
-}
+  Rocky: {
+    year: 1976,
+    director: "John G. Avildsen",
+  },
+  "Rocky II": {
+    year: 1979,
+    director: "Sylvester Stallone",
+  },
+  "Rocky III": {
+    year: 1982,
+    director: "Sylvester Stallone",
+  },
+};
 
 // Obtener el año de grabación de Rocky III
-console.log(rocky_movies["Rocky III"].year)
+console.log(rocky_movies["Rocky III"].year);
 
 const tierra = {
-    "America": {
-        "Mexico": {
-            estados: [
-                "CDMX",
-                "Guadalajara",
-                "Monterrey",
-                "SLP"
-            ]
-        },
-        "Estados Unidos": {
-            estados: [
-                "California",
-                "Texas",
-                "Florida",
-                "New York"
-            ]
-        }
+  continentes: {
+    America: {
+      Mexico: {
+        estados: ["CDMX", "Guadalajara", "Monterrey", "SLP"],
+      },
+      "Estados Unidos": {
+        estados: ["California", "Texas", "Florida", "New York"],
+      },
     },
-    "Europa": {
-        "España": {
-            ciudades: [
-                "Madrid",
-                "Barcelona",
-                "Valencia",
-                "Sevilla"
-            ]
-        },
-        "Francia": {
-            ciudades: [
-                "París",
-                "Lyon",
-                "Marsella",
-                "Niza"
-            ]
-        }
+    Europa: {
+      España: {
+        ciudades: ["Madrid", "Barcelona", "Valencia", "Sevilla"],
+      },
+      Francia: {
+        ciudades: ["París", "Lyon", "Marsella", "Niza"],
+      },
     },
-    contintentes() {
-
-    },
-
-    paises() {
-
-    },
-
-    estados() {
-
-    },
-
-    ciudades() {
-
+  },
+  contintentes() {
+    continentes_list = []
+    for (continente in this.continentes) {
+        continentes_list.push(continente)
     }
-}
+    console.log(`Continentes: ${continentes_list}`)
+
+    //console.log(`Continentes: ${Object.keys(this.continentes)}`)
+  },
+
+  paises() {},
+
+  estados() {},
+
+  ciudades() {},
+};
 
 // Imprimir de la siguiente forma
 
@@ -87,7 +89,7 @@ const tierra = {
 // Estados: CDMX, Guadalajara..
 // Ciudades: Madrid, Barcelona...
 
-tierra.contintentes()
-tierra.paises()
-tierra.estados()
-tierra.ciudades()
+tierra.contintentes();
+tierra.paises();
+tierra.estados();
+tierra.ciudades();
